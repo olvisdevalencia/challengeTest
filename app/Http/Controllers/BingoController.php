bb90d8c464accac3d08551912dc15585c16861eb
Olvis Quintana <olvis@fabrik.pe>
2020-07-18 15:50:30 -0500

    Card model , controller method to play and check for a new game or continue calling new numbers to call play

--- app/Http/Controllers/BingoController.php
+++ app/Http/Controllers/BingoController.php
@@ -10,6 +10,9 @@ class BingoController extends Controller
     public $results;
     public $lastNumber;
 
+    /**
+     * Method to initialize array of numbers and start a new game
+     */
     private function initializeGame()
     {
         $this->results = [];
@@ -20,5 +23,55 @@ class BingoController extends Controller
         unset($this->results[0]);
 
         $this->lastNumber = 0;
+    }
+
+    /**
+     * Method to check it game have been finished
+     */
+    private function weFinish()
+    {
+        $finished = true;
+        foreach($this->results as $result)
+        {
+            if($result == 0)
+            {
+                $finished = false;
+                break;
+            }
+        }
+        return $finished;
+    }
+
+    /**
+     * Method to play a new game and check for the new number
+     */
+    public function newGame()
+    {
+        if(!$this->weFinish())
+        {
+            $found = false;
+
+            do
+            {
+                $currentNumber = \random_int(1, 75);
+
+                if($this->results[$currentNumber] == 0)
+                {
+                    $found = true;
+                }
+
+            }while($found == false);
+
+            $this->lastNumber = $currentNumber;
+            $this->results[$currentNumber] = 1;
+
+            usleep(1 * 1000000);
+        }
+        else
+        {
+            $this->initializeGame();
+            usleep(1 * 1000000);
+        }
+
     }
 }
